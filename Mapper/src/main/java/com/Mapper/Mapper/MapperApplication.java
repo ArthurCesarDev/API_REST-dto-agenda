12ff1777c025201af2b9cc854af8d2c74de4d396
ArthurCesarDev <arthurcesarx@outlook.com>
2022-08-01 22:10:57 -0300

    Sistema Configurado

---
+++ Mapper/src/main/java/com/Mapper/Mapper/MapperApplication.java
@@ -0,0 +1,13 @@
+package com.Mapper.Mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperApplication.class, args);
+	}
+
+}
---
+++ Mapper/src/main/java/com/Mapper/domain/entity/Agenda.java
@@ -0,0 +1,5 @@
+package com.Mapper.domain.entity;
+
+public class Agenda {
+
+}
